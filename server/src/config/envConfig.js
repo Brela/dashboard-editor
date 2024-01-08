3650e58d751691a0759332ad2294bc7ad15a0c32
Brela <claybreland1@gmail.com>
2024-01-08 12:37:54 -0600

    fix cors origins

--- server/src/config/envConfig.js
+++ server/src/config/envConfig.js
@@ -1,10 +1,9 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 
 export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
 export const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
-export const CORS_ORIGIN = process.env.CORS_ORIGIN;
-export const PORT = process.env.PORT
+export const CORS_ORIGINS = process.env.CORS_ORIGINS;
+export const PORT = process.env.PORT;
 export const IS_DEV_MODE = process.env.IS_DEV_MODE;
-
