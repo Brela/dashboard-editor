5a7a9035ad8e0232c50fa332ea3f2df1e42d2ca0
Brela <claybreland1@gmail.com>
2024-01-09 13:26:37 -0600

    origins

--- server/src/server.js
+++ server/src/server.js
@@ -18,16 +18,16 @@ const allowedOrigins = CORS_ORIGINS.split(",");
 
 const corsOptions = {
   // temporarily allow all origins
-  /*    origin: function (origin, callback) {
-    callback(null, true); // Allow all origins
-  }, */
   origin: function (origin, callback) {
+    callback(null, true); // Allow all origins
+  },
+  /* origin: function (origin, callback) {
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
     }
-  },
+  }, */
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
